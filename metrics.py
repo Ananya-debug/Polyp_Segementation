b2437c3bb147cfdd0afd38e23faa1d3dd3ae8aea
Ananya Chakraborty <ac2236177@gmail.com>
2026-04-01 15:37:46 +0530

    Refactor dice_coef and add precision/recall functions
    Refactor dice_coef function for readability and add precision and recall functions.

--- metrics.py
+++ metrics.py
@@ -1,12 +1,34 @@
-import numpy as np
 import tensorflow as tf
 
 smooth = 1e-15
+
 def dice_coef(y_true, y_pred):
-    y_true = tf.keras.layers.Flatten()(y_true)
-    y_pred = tf.keras.layers.Flatten()(y_pred)
+    y_true = tf.reshape(y_true, [-1])
+    y_pred = tf.reshape(y_pred, [-1])
     intersection = tf.reduce_sum(y_true * y_pred)
-    return (2. * intersection + smooth) / (tf.reduce_sum(y_true) + tf.reduce_sum(y_pred) + smooth)
+    return (2. * intersection + smooth) / (
+        tf.reduce_sum(y_true) + tf.reduce_sum(y_pred) + smooth
+    )
 
 def dice_loss(y_true, y_pred):
     return 1.0 - dice_coef(y_true, y_pred)
+
+def precision(y_true, y_pred):
+    y_true = tf.reshape(y_true, [-1])
+    y_pred = tf.reshape(y_pred, [-1])
+    y_pred = tf.round(y_pred)
+
+    tp = tf.reduce_sum(y_true * y_pred)
+    fp = tf.reduce_sum((1 - y_true) * y_pred)
+
+    return tp / (tp + fp + 1e-7)
+
+def recall(y_true, y_pred):
+    y_true = tf.reshape(y_true, [-1])
+    y_pred = tf.reshape(y_pred, [-1])
+    y_pred = tf.round(y_pred)
+
+    tp = tf.reduce_sum(y_true * y_pred)
+    fn = tf.reduce_sum(y_true * (1 - y_pred))
+
+    return tp / (tp + fn + 1e-7)
